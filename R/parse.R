3c9e46b79adaca947688b8afe77d668ce7ff2146
kamal26 <kamal26@student.ubc.ca>
2021-03-10 02:41:08 -0500

    Add core of parse_lab function

--- R/parse.R
+++ R/parse.R
@@ -11,5 +11,41 @@
 #' \dontrun{
 #' parse_lab()
 #' }
-parse_lab <- function(notebook = NULL) {
-}
+#'
+
+parse_lab <- function(notebook) {
+
+  # Extracting the fileextension
+  file_ext <- getExtension(notebook)
+
+  # check whether the file is jupyter notebook or Rmarkdown
+  if (file_ext == "ipynb") {
+  
+  # read jupyternotebook as a json file and parse markdown contents
+  py_parse = jsonlite::read_json(notebook)
+  cells = py_parse$cells
+  source <- character()
+  for (cell in cells) {
+    if (cell$cell_type == "markdown") {
+      cell_content <- unlist(cell$source, use.names = FALSE)
+      cell_content <- paste(cell_content, collapse = '')
+      source[length(source) + 1] <- cell_content
+    }
+  }
+  } else if (file_ext == "Rmd") {
+
+    # read Rmarkdown file and parse markdown contents
+    rmd_f <- readLines(notebook)
+    cell_content <- paste(rmd_f, collapse = '')
+    splitted <- stringr::str_split(cell_content, "```", simplify = TRUE)
+    code_bool <- startsWith(splitted, "{python") | startsWith(splitted, "{r")
+    source <- splitted[!code_bool]
+  }
+  return(source)
+  }
+
+  # helper function for extracting the file extension
+  getExtension <- function(file){
+    ex <- strsplit(basename(file), split="\\.")[[1]]
+    return(ex[-1])
+  }
